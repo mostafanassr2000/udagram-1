e82710ba9a836fe558e8925c0babcd11fe76e92b
Mostafa Nassr <62142244+mostafanassr2000@users.noreply.github.com>
2022-05-04 05:00:52 +0200

    new

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,16 +1,13 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// ENV variables 
-// - AWS_ACCESS_KEY_ID
-// - AWS_SECRET_ACCESS_KEY
-// Are Also needed
 
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   port: Number(process.env.PORT),
+  dbPort: Number(process.env.DB_PORT),
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
